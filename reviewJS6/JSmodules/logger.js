e3172f89613afd503f84194907b183ce3c8aa7c8
PhongDoan200299 <97325515+PhongDoan200299@users.noreply.github.com>
2024-02-26 21:42:18 +0700

    second commit

--- reviewJS6/JSmodules/logger.js
+++
@@ -1,9 +0,0 @@
-import { TYPE_LOG} from './constants.js';
-
-function logger (log, type = 'log'){
-    console[type](log);
-}
-
-
-export default logger;
-
--- reviewJS6/JSmodules/myscripts.js
+++ reviewJS6/JSmodules/myscripts.js
@@ -1,11 +1,24 @@
 // Modules: tư tưởng, khi bóc tách ra xử lý 1 nghiệp vụ cụ thể ra 1 nơi riêng, có thể gọi nó là module. Thường tách ra những file và tập hợp những file
 //Modules: Import / Export
 
-import logger,{
+import logger from './logger/index.js';
+// dang import cái module dang export default
+
+
+
+import {
     TYPE_LOG,
     TYPE_WARN,
     TYPE_ERROR
-} from './logger.js';
+} from './constants.js';
+// đang import cái module đang export 
+
+// import constants from './constants.js';
+// Sai vì module constants không export default
+
+import * as constants from './constants.js';
+
+console.log(constants);
 
 // console.log( logger);
-logger('Test message...', TYPE_ERROR);
+logger('Test message...', TYPE_LOG);
